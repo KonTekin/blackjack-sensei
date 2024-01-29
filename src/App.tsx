874e1fd4fbb433e40821022f69c6e5798c07c1bc
Konstantin Tekin <131813083+KonTekin@users.noreply.github.com>
2024-01-28 19:10:31 -0500

    Added a card flip animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
 	return (
 		<>
-			<div>Blackjack home page</div>
+			<img src='assets/backside_card' alt='' />
+
+			<div className='playing-card'>
+				<div className='flip-card'>
+					<div className='front-side'>
+						<img src='src/assets/ace_clubs.png' alt='' />
+					</div>
+					<div className='back-side'>
+						<img src='src/assets/backside_card.png' alt='' />
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
