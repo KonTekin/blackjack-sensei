09065d0af1c511908a028769616f36129df26525
Konstantin Tekin <131813083+KonTekin@users.noreply.github.com>
2024-01-29 20:17:04 -0500

    Testing card dealing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [isFlipped, setIsFlipped] = useState(false);
 	return (
 		<>
-			<img src='assets/backside_card' alt='' />
-
 			<div className='playing-card'>
-				<div className='flip-card'>
+				<div className={isFlipped ? `flip-card deal-card` : `flip-card`}>
 					<div className='front-side'>
 						<img src='src/assets/ace_clubs.png' alt='' />
 					</div>
@@ -15,6 +15,12 @@ function App() {
 					</div>
 				</div>
 			</div>
+
+			<div className='player-hand'></div>
+
+			<button className='general-btn' onClick={() => setIsFlipped(!isFlipped)}>
+				Flip
+			</button>
 		</>
 	);
 }
