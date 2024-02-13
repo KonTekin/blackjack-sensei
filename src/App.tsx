1ec33a2ddd22ece422bd6f49ea77a660b3ff90e0
Konstantin Tekin <131813083+KonTekin@users.noreply.github.com>
2024-02-13 17:34:58 -0500

    Ability to set bet amount and expanded the context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { GameContext } from "./context/game";
 import "./App.css";
 
 function App() {
-	const [isFlipped, setIsFlipped] = useState(false);
-	
+	const [isFlipped] = useState(false);
+	const game = useContext(GameContext);
+	const handleBetAction = () => {};
+	const increaseBet = () => {
+		game.setPlayerBet((prevPlayerBet) => {
+			return prevPlayerBet < game.playerBalance
+				? prevPlayerBet + 5
+				: prevPlayerBet;
+		});
+	};
+	const decreaseBet = (amount: number) => {
+		game.setPlayerBet((prevPlayerBet) => {
+			return prevPlayerBet > amount ? prevPlayerBet - amount : 0;
+		});
+	};
 	return (
 		<div className="app-container">
 			<div className="dealer-hand-container">Dealer hand</div>
@@ -32,12 +46,34 @@ function App() {
 			<div className="player-hit-hand-container">hit cards</div>
 			<div className="card-deck-container">Deck of Cards</div>
 			<div className="player-options-container">
-				<button
+				{game.isMakingBet && (
+					<>
+						<div className="bet-actions">
+							<div>Current Balance: {game.playerBalance}</div>
+							<button type="button" onClick={increaseBet}>
+								increase
+							</button>
+							<div>{game.playerBet}</div>
+							<button type="button" onClick={() => decreaseBet(5)}>
+								decrease
+							</button>
+						</div>
+						<button
+							type="button"
+							className="general-btn"
+							onClick={handleBetAction}
+						>
+							Place Bet
+						</button>
+					</>
+				)}
+				{/* <button
+					type="button"
 					className="general-btn"
 					onClick={() => setIsFlipped(!isFlipped)}
 				>
 					Flip
-				</button>
+				</button> */}
 			</div>
 		</div>
 	);
--- src/context/game.tsx
+++ src/context/game.tsx
@@ -1,24 +1,61 @@
-import { createContext, ReactNode, useState } from "react";
-import * as DECK from "../deck-of-cards.json";
+import {
+	createContext,
+	Dispatch,
+	ReactNode,
+	SetStateAction,
+	useState,
+} from "react";
+import { Deck } from "../deck-of-cards";
 
 interface Props {
 	children?: ReactNode;
 }
 
-export const GameContext = createContext({
-	gameDeck: [{}],
+interface ICard {
+	suit: string;
+	value: number;
+}
+
+interface IGameContext {
+	gameDeck: ICard[];
+	playerHand: ICard[];
+	dealerHand: ICard[];
+	isMakingBet: boolean;
+	playerBet: number;
+	playerBalance: number;
+	setGameDeck: Dispatch<SetStateAction<ICard[]>>;
+	setDealerHand: Dispatch<SetStateAction<ICard[]>>;
+	setPlayerHand: Dispatch<SetStateAction<ICard[]>>;
+	setIsMakingBet: Dispatch<SetStateAction<boolean>>;
+	setPlayerBet: Dispatch<SetStateAction<number>>;
+}
+
+const blankHand: ICard = {
+	suit: "none",
+	value: 0,
+};
+
+export const GameContext = createContext<IGameContext>({
+	gameDeck: [],
 	playerHand: [],
 	dealerHand: [],
-	isMakingBet: false,
+	isMakingBet: true,
 	playerBet: 0,
+	playerBalance: 100,
+	setGameDeck: () => {},
+	setDealerHand: () => {},
+	setPlayerHand: () => {},
+	setIsMakingBet: () => {},
+	setPlayerBet: () => {},
 });
 
 export const GameProvider = ({ children }: Props) => {
-	const [gameDeck, setGameDeck] = useState(DECK);
-	const [playerHand, setPlayerHand] = useState([]);
-	const [dealerHand, setDealerHand] = useState([]);
+	const [gameDeck, setGameDeck] = useState(Deck);
+	const [playerHand, setPlayerHand] = useState([blankHand]);
+	const [dealerHand, setDealerHand] = useState([blankHand]);
 	const [isMakingBet, setIsMakingBet] = useState(true);
 	const [playerBet, setPlayerBet] = useState(0);
+	const [playerBalance, setPlayerBalance] = useState(100);
 
 	const value = {
 		gameDeck,
@@ -26,11 +63,13 @@ export const GameProvider = ({ children }: Props) => {
 		dealerHand,
 		isMakingBet,
 		playerBet,
+		playerBalance,
 		setDealerHand,
 		setGameDeck,
 		setIsMakingBet,
 		setPlayerBet,
 		setPlayerHand,
+		setPlayerBalance,
 	};
 
 	return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
